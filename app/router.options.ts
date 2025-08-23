d107506d8850cbc6a71578e76dda452ff09a3492
odiloviccc <odiloviccc@gmail.com>
2025-08-23 17:10:24 +0500

    fix

--- app/router.options.ts
+++ app/router.options.ts
@@ -9,31 +9,6 @@ export default <RouterConfig>{
                 name: RouterPaths.DASHBOARD,
                 component: () => import('~/pages/index.vue'),
             },
-            {
-                path: "/test",
-                name: RouterPaths.DEV_TEST,
-                component: () => import('~/pages/dev/test.vue'),
-            },
-            {
-                path: '/auth/',
-                component: () => import('~/pages/auth/layout.vue'),
-                redirect: RouterPaths.LOGIN,
-                meta: {
-                    layout: "auth"
-                },
-                children: [
-                    {
-                        path: 'login',
-                        component: () => import('~/pages/auth/login/index.vue'),
-                        name: RouterPaths.LOGIN,
-                    },
-                    {
-                        path: 'register',
-                        component: () => import('~/pages/auth/register/index.vue'),
-                        name: RouterPaths.REGISTER,
-                    }
-                ]
-            }
         ];
     }
 }
